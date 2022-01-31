5d1f777f4b8e032f6ad97c7f8679b91e3ea6e34f
Ahmed Hossam <1318039@gmail.com>
2022-01-31 18:21:50 +0200

    Build getBooks

--- src/components/Book/BookContainer.js
+++ src/components/Book/BookContainer.js
@@ -1,16 +1,24 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import BookInfo from "./BookInfo";
 import BooksList from "./BooksList";
-
 import "./book.css";
+import { useDispatch, useSelector } from "react-redux";
+import { getBooks } from "../../store/bookSlice";
 
 const PostContainer = () => {
+  const dispatch = useDispatch();
+  const { books, isLoading } = useSelector((state) => state.books);
+
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
+
   return (
     <Fragment>
       <hr className="my-5" />
       <div className="row">
         <div className="col-12">
-          <BooksList />
+          <BooksList books={books} isLoading={isLoading} />
         </div>
         <div className="col-12 side-line mt-5">
           <BookInfo />
--- src/store/bookSlice.js
+++ src/store/bookSlice.js
@@ -1,11 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getBooks = createAsyncThunk(
+  "books/getBooks",
+  async (args, thunkAPI) => {
+    const res = await fetch("http://localhost:3005/books");
+    const data = await res.json();
+
+    return data;
+  }
+);
 
 const booksSlice = createSlice({
   name: "books",
   initialState: {
     books: null,
+    isLoading: false,
   },
-  reducer: {},
+  extraReducers: {
+    [getBooks.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+    [getBooks.rejected]: (state, action) => {
+      state.isLoading = false;
+    },
+    [getBooks.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.books = action.payload;
+    },
+  },
 });
 
 export default booksSlice.reducer;
